f07b1ba3bd02d41be763bc75eb59d3560f58975a
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-05-26 20:05:28 +0200

    setting up application routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { FightGridComponent } from './pages/fight-grid/fight-grid.component';
 import { FlightDetailsComponent } from './pages/flight-details/flight-details.component';
+import { FlightSummaryComponent } from './pages/flight-summary/flight-summary.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -24,6 +25,7 @@ const routes: Routes = [
   { path: 'flight-search', component: FlightSearchComponent},
   { path: 'flight-grid', component: FightGridComponent },
   { path: 'flight-details', component: FlightDetailsComponent },
+  { path: 'flight-summary', component: FlightSummaryComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
--- src/app/pages/flight-details/flight-details.component.ts
+++ src/app/pages/flight-details/flight-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-flight-details',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./flight-details.component.scss']
 })
 export class FlightDetailsComponent {
+  
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    
+  }
+  redirectToLogin(path: string) {
+    this.router.navigate(['/login']);
+  }
 }
+
