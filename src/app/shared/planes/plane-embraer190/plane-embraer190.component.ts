f23178cbfbda87674dd1d45b9caa21d31400a68b
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-05-31 15:11:17 +0200

    array with selected seats on the plane

--- src/app/shared/planes/plane-embraer190/plane-embraer190.component.ts
+++ src/app/shared/planes/plane-embraer190/plane-embraer190.component.ts
@@ -9,14 +9,33 @@ import { FlightService } from 'src/app/services/flight.service';
 export class PlaneEmbraer190Component {
   seats: string[] = ['A', 'B', 'C', 'D'];
   rows: number[] = Array.from({ length: 10 }, (_, index) => index + 1);
+  selectedSeats: string[] = [];
 
   @Input() flightId!: number;
 
   constructor(private flightService: FlightService) { }
 
-  isReserved(seat: string) : boolean {
+  isReserved(seat: string): boolean {
     const result = this.flightService.isReserved(+this.flightId, seat);
     return result;
   }
-}
 
+  selectSeats(seat: string): void {
+    if (!this.isReserved(seat)) {
+      if (this.isSelected(seat)) {
+        const index = this.selectedSeats.indexOf(seat);
+        if (index !== -1) {
+          this.selectedSeats.splice(index, 1);
+        }
+        
+      } else {
+        this.selectedSeats.push(seat);
+      }
+      console.log(this.selectedSeats);
+    }
+  }
+
+  isSelected(seat: string): boolean {
+    return this.selectedSeats.includes(seat);
+  }
+}
