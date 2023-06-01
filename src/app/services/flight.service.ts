edf738e608eabc5e12051602e0164e7f7c2ab871
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-06-01 14:31:41 +0200

    changes to rwd

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -16,46 +16,13 @@ export class FlightService {
     const monthsAhead = 6;
     this.flights = this.flightGeneratorService.generateFlights(startDate, monthsAhead);
     console.log(this.flights);
-    
-    // this.flights = [
-    //   { id: 1, from: 'Gdansk', to: 'Vienna', departureDate: this.getDateShiftedByDays(1), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 2, from: 'Vienna', to: 'Roma', departureDate: this.getDateShiftedByDays(5), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 3, from: 'Cracow', to: 'Gdansk', departureDate: this.getDateShiftedByDays(10), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 4, from: 'Gdansk', to: 'Vienna', departureDate: this.getDateShiftedByDays(18), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 5, from: 'Vienna', to: 'Roma', departureDate: this.getDateShiftedByDays(25), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 6, from: 'Berlin', to: 'Gdansk', departureDate: this.getDateShiftedByDays(30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 7, from: 'Gdansk', to: 'Vienna', departureDate: this.getDateShiftedByDays(40), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 8, from: 'Vienna', to: 'Roma', departureDate: this.getDateShiftedByDays(45), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 9, from: 'Berlin', to: 'Gdansk', departureDate: this.getDateShiftedByDays(2*30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 10, from: 'Gdansk', to: 'Vienna', departureDate: this.getDateShiftedByDays(3*30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 11, from: 'Vienna', to: 'Roma', departureDate: this.getDateShiftedByDays(4*30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 12, from: 'Boston', to: 'Gdansk', departureDate: this.getDateShiftedByDays(5*30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    //   { id: 13, from: 'Boston', to: 'Gdansk', departureDate: this.getDateShiftedByDays(6*30), class: 'Economy', totalSeatsCount: 40
-    //   , reservedSeats: ['1A', '1B', '2D'] },
-    // ];
+
   }  
 
   isReserved(flightId: number, seat: string): boolean {
     return this.flights.some(flight => flight.id === flightId && flight.reservedSeats.indexOf(seat) > -1)
   }
 
-  // private getDateShiftedByDays(daysToAdd: number) {
-  //   const result = new Date();
-  //   result.setDate(result.getDate() + daysToAdd)
-  //   return result.toJSON().slice(0, 10);
-  // }
 
   getAvailableFromFlights() : string[] {
     return this.flights.map(flight => flight.from).filter(this.onlyUnique).sort();
